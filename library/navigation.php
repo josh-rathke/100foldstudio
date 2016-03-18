6f7f66e348e8c0c25148e1d9d41bdb3cea6ac579
Josh Rathke <josh.rathke@ywammontana.org>
2016-03-18 14:15:24 -0600

    Bug fix Menus

--- library/navigation.php
+++ library/navigation.php
@@ -7,15 +7,12 @@
  * @subpackage FoundationPress
  * @since FoundationPress 1.0
  */
-
 register_nav_menus(array(
 	'top-bar'             => 'Top Bar',
 	'mobile-off-canvas'   => 'Mobile',
-    'join-us-menu'        => 'Join Us',
+    'join-us'             => 'Join Us',
     'copyright-bar'       => 'Copyright Bar',
 ));
-
-
 /**
  * Left top bar
  * http://codex.wordpress.org/Function_Reference/wp_nav_menu
@@ -38,14 +35,18 @@ if ( ! function_exists( '_100foldstudio_top_bar' ) ) {
 	    ));
 	}
 }
-
-
 /**
- *  Add Custom Menu Items
- *  These functions add custom menu items to specific menus.
+ *  Add Logo to Top Bar
  */
 add_filter('wp_nav_menu_items','add_custom_menu_items', 10, 2);
 function add_custom_menu_items( $nav, $args ) {
+    
+    // Logo to Top Bar
+    if( $args->theme_location == 'top-bar' ) {
+        //$newmenuitem = '<img src="' . get_bloginfo("template_url") . '/assets/img/logo_on_white_only.png" />';
+        //$nav = $newmenuitem.$nav;
+        return $nav;
+    }
     
     // Add Copyright to Copyright Bar
     if( $args->theme_location == 'copyright-bar' ) {
@@ -54,8 +55,6 @@ function add_custom_menu_items( $nav, $args ) {
         return $nav;
     }
 }
-
-
 /**
  * Mobile off-canvas
  */
@@ -77,8 +76,6 @@ if ( ! function_exists( '_100foldstudio_mobile_off_canvas' ) ) {
 	    ));
 	}
 }
-
-
 /**
  * Join Us
  */
@@ -88,18 +85,16 @@ if ( ! function_exists( '_100foldstudio_join_us' ) ) {
 	        'container' => false,                           // Remove nav container
 	        'container_class' => '',                        // Class of container
 	        'menu' => '',                                   // Menu name
-	        'menu_class' => 'join-us-list',          // Adding custom nav class
-	        'theme_location' => 'join-us-menu',            // Where it's located in the theme
+	        'menu_class' => 'join-us-list',                 // Adding custom nav class
+	        'theme_location' => 'join-us',                  // Where it's located in the theme
 	        'before' => '',                                 // Before each link <a>
 	        'after' => '',                                  // After each link </a>
 	        'link_before' => '',                            // Before each link text
 	        'link_after' => '',                             // After each link text
-	        'depth' => 1,  
+	        'depth' => 1,                                   // Limit the depth of the nav
 	    ));
 	}
 }
-
-
 /**
  * Copyright Bar
  */
@@ -109,7 +104,7 @@ if ( ! function_exists( '_100foldstudio_copyright_bar' ) ) {
 	        'container' => false,                           // Remove nav container
 	        'container_class' => '',                        // Class of container
 	        'menu' => '',                                   // Menu name
-	        'menu_class' => 'copyright-bar-list',          // Adding custom nav class
+	        'menu_class' => 'copyright0-bar-list',          // Adding custom nav class
 	        'theme_location' => 'copyright-bar',            // Where it's located in the theme
 	        'before' => '',                                 // Before each link <a>
 	        'after' => '',                                  // After each link </a>
@@ -119,9 +114,6 @@ if ( ! function_exists( '_100foldstudio_copyright_bar' ) ) {
 	    ));
 	}
 }
-
-
-
 /**
  * Add support for buttons in the top-bar menu:
  * 1) In WordPress admin, go to Apperance -> Menus.
@@ -133,67 +125,50 @@ if ( ! function_exists( '_100foldstudio_add_menuclass' ) ) {
 	function _100foldstudio_add_menuclass($ulclass) {
 	    $find = array('/<a rel="button"/', '/<a title=".*?" rel="button"/');
 	    $replace = array('<a rel="button" class="button"', '<a rel="button" class="button"');
-
 	    return preg_replace( $find, $replace, $ulclass, 1 );
 	}
 	add_filter( 'wp_nav_menu','_100foldstudio_add_menuclass' );
 }
-
-
 /**
  * Adapted for Foundation from http://thewebtaylor.com/articles/wordpress-creating-breadcrumbs-without-a-plugin
  * @param bool $showhome should the breadcrumb be shown when on homepage (only one deactivated entry for home).
  * @param bool $separatorclass should a separator class be added (in case :before is not an option).
  */
 function _100foldstudio_breadcrumb($showhome = true, $separatorclass = false) {
-
     // Settings
     $separator  = '&gt;';
     $id         = 'breadcrumbs';
     $class      = 'breadcrumbs';
     $home_title = 'Homepage';
-
     // Get the query & post information
     global $post,$wp_query;
     $category = get_the_category();
-
     // Build the breadcrums
     echo '<ul id="' . $id . '" class="' . $class . '">';
-
     // Do not display on the homepage
     if ( ! is_front_page() ) {
-
         // Home page
         echo '<li class="item-home"><a class="bread-link bread-home" href="' . get_home_url() . '" title="' . $home_title . '">' . $home_title . '</a></li>';
         if ( $separatorclass ) {
             echo '<li class="separator separator-home"> ' . $separator . ' </li>';
         }
-
         if ( is_single() ) {
-
             // Single post (Only display the first category)
             echo '<li class="item-cat item-cat-' . $category[0]->term_id . ' item-cat-' . $category[0]->category_nicename . '"><a class="bread-cat bread-cat-' . $category[0]->term_id . ' bread-cat-' . $category[0]->category_nicename . '" href="' . get_category_link($category[0]->term_id) . '" title="' . $category[0]->cat_name . '">' . $category[0]->cat_name . '</a></li>';
             if ( $separatorclass ) {
                 echo '<li class="separator separator-' . $category[0]->term_id . '"> ' . $separator . ' </li>';
             }
             echo '<li class="item-current item-' . $post->ID . '"><strong class="bread-current bread-' . $post->ID . '" title="' . get_the_title() . '">' . get_the_title() . '</strong></li>';
-
         } else if ( is_category() ) {
-
             // Category page
             echo '<li class="item-current item-cat-' . $category[0]->term_id . ' item-cat-' . $category[0]->category_nicename . '"><strong class="bread-current bread-cat-' . $category[0]->term_id . ' bread-cat-' . $category[0]->category_nicename . '">' . $category[0]->cat_name . '</strong></li>';
-
         } else if ( is_page() ) {
-
             // Standard page
             if ( $post->post_parent ) {
-
                 // If child page, get parents
                 $anc = get_post_ancestors( $post->ID );
-
                 // Get parents in the right order
                 $anc = array_reverse( $anc );
-
                 // Parent page loop
                 $parents = '';
                 foreach ( $anc as $ancestor ) {
@@ -202,88 +177,63 @@ function _100foldstudio_breadcrumb($showhome = true, $separatorclass = false) {
                         $parents .= '<li class="separator separator-' . $ancestor . '"> ' . $separator . ' </li>';
                     }
                 }
-
                 // Display parent pages
                 echo $parents;
-
                 // Current page
                 echo '<li class="current item-' . $post->ID . '">' . get_the_title() . '</li>';
-
             } else {
-
                 // Just display current page if not parents
                 echo '<li class="current item-' . $post->ID . '"> ' . get_the_title() . '</li>';
-
             }
         } else if ( is_tag() ) {
-
             // Tag page
             // Get tag information
             $term_id = get_query_var('tag_id');
             $taxonomy = 'post_tag';
             $args = 'include=' . $term_id;
             $terms = get_terms($taxonomy, $args);
-
             // Display the tag name
             echo '<li class="current item-tag-' . $terms[0]->term_id . ' item-tag-' . $terms[0]->slug . '">' . $terms[0]->name . '</li>';
-
         } elseif ( is_day() ) {
-
             // Day archive
             // Year link
             echo '<li class="item-year item-year-' . get_the_time('Y') . '"><a class="bread-year bread-year-' . get_the_time('Y') . '" href="' . get_year_link(get_the_time('Y')) . '" title="' . get_the_time('Y') . '">' . get_the_time('Y') . ' Archives</a></li>';
             if ( $separatorclass ) {
                 echo '<li class="separator separator-' . get_the_time('Y') . '"> ' . $separator . ' </li>';
             }
-
             // Month link
             echo '<li class="item-month item-month-' . get_the_time('m') . '"><a class="bread-month bread-month-' . get_the_time('m') . '" href="' . get_month_link(get_the_time('Y'), get_the_time('m')) . '" title="' . get_the_time('M') . '">' . get_the_time('M') . ' Archives</a></li>';
             if ( $separatorclass ) {
                 echo '<li class="separator separator-' . get_the_time('m') . '"> ' . $separator . ' </li>';
             }
-
             // Day display
             echo '<li class="current item-' . get_the_time('j') . '">' . get_the_time('jS') . ' ' . get_the_time('M') . ' Archives</li>';
-
         } else if ( is_month() ) {
-
             // Month Archive
             // Year link
             echo '<li class="item-year item-year-' . get_the_time('Y') . '"><a class="bread-year bread-year-' . get_the_time('Y') . '" href="' . get_year_link(get_the_time('Y')) . '" title="' . get_the_time('Y') . '">' . get_the_time('Y') . ' Archives</a></li>';
             if ( $separatorclass ) {
                 echo '<li class="separator separator-' . get_the_time('Y') . '"> ' . $separator . ' </li>';
             }
-
             // Month display
             echo '<li class="item-month item-month-' . get_the_time('m') . '">' . get_the_time('M') . ' Archives</li>';
-
         } else if ( is_year() ) {
-
             // Display year archive
             echo '<li class="current item-current-' . get_the_time('Y') . '">' . get_the_time('Y') . ' Archives</li>';
-
         } else if ( is_author() ) {
-
             // Auhor archive
             // Get the author information
             global $author;
             $userdata = get_userdata($author);
-
             // Display author name
             echo '<li class="current item-current-' . $userdata->user_nicename . '">Author: ' . $userdata->display_name . '</li>';
-
         } else if ( get_query_var('paged') ) {
-
             // Paginated archives
             echo '<li class="current item-current-' . get_query_var('paged') . '">'.__('Page') . ' ' . get_query_var('paged') . '</li>';
-
         } else if ( is_search() ) {
-
             // Search results page
             echo '<li class="current item-current-' . get_search_query() . '">Search results for: ' . get_search_query() . '</li>';
-
         } elseif ( is_404() ) {
-
             // 404 page
             echo '<li>Error 404</li>';
         }
@@ -294,5 +244,4 @@ function _100foldstudio_breadcrumb($showhome = true, $separatorclass = false) {
     }
     echo '</ul>';
 }
-
 ?>
